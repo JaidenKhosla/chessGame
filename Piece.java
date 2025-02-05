8deb11591ccbfa053b14aad0456fecc9cc0157a0
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-02-05 20:34:41 +0000

    added correctDirection

--- Piece.java
+++ Piece.java
@@ -4,25 +4,33 @@ public class Piece  {
     private int col;
     private int range = Integer.MAX_VALUE;
     private String icon;
+    private boolean canJump;
+    private int[][] directions;
 
-    public Piece(String icon,int row, int col){
+    public Piece(String icon,int row, int col, boolean canJump, int[][] directions){
         this.icon = icon;
         this.row = row;
         this.col = col;
+        this.canJump = canJump;
+        this.directions = directions;
     }
 
-    public Piece(String icon, int row, int col, int range){
+    public Piece(String icon, int row, int col, int range, boolean canJump, int[][] directions){
         this.icon = icon;
         this.row = row;
         this.col = col;
         this.range = range;
+        this.canJump = canJump;
+        this.directions = directions;
+
     }
 
     public void move(int y, int x) throws Exception{
         int newRow = this.row+y;
         int newCol = this.col+x;
         
-        if(this.distanceTo(y, x) > this.range) throw new Exception(String.format("(%d, %d) is out of the piece's range.", y,x));
+
+        if(this.distanceTo(y, x) > this.range) throw new Exception(String.format("(%d, %d) is out of the piece's range or direction or both.", y,x));
 
 
         Board.grid[this.row][this.col] = null;
@@ -34,6 +42,19 @@ public class Piece  {
 
     public int distanceTo(int y, int x){
         return (int) Math.floor(Math.sqrt(Math.pow(this.row-y,2)+Math.pow(this.col-x,2)));
+    }
+
+    public boolean correctDirection(int y, int x){
+        for(int[] direction : directions){
+            int directionY = direction[0];
+            int directionX = direction[1];
+
+            int y1 = Math.abs(y-this.row)%directionY;
+            int x1 = Math.abs(x-this.col)%directionX;
+
+            if(y1==0&&x1==0) return true;
+        }
+        return false;
     }
 
     public String toString(){
